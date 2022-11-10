8f1c00b854817e89d8a13f3c4f89b9d64c059453
kildan <kildan325@gmail.com>
2022-11-10 10:53:11 +0300

    Add header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,19 +1,25 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { Logo } from '../Logo'
-import './style.css'
+import { Button } from 'antd'
+import './Header.scss'
 
 export function Header(): JSX.Element {
     return (
         <header>
             <Logo />
-            <nav>
+            <nav className='nav_head'>
                 <ul className="navbar">
-                    <li><Link to={"/"}>Главная</Link> </li>
+                    <li><Link to={"/"} className='main main_active'>Главная</Link> </li>
                     <li><Link to={"/poll"}>Опрос</Link></li>
                     <li><Link to={"/about"}>О нас</Link></li>
                 </ul>
+                <div className='icons_header'>
+                    <Button  className='btn_login'>Войти</Button>
+                    <img src='src\Assets\cart.png' className='btn_cart'/>
+                </div>
             </nav>
+           
             {/* add login , profile and basket*/}
         </header>
     )
