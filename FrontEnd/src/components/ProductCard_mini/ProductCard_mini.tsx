7ab3fba5e676113d9f9a8b84e00b72f013f8dbd0
kildan <kildan325@gmail.com>
2022-11-12 19:33:17 +0300

    Change ProductCard_mini

--- FrontEnd/src/components/ProductCard_mini/ProductCard_mini.tsx
+++ FrontEnd/src/components/ProductCard_mini/ProductCard_mini.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { TCard } from '../../types'
+import { Button } from 'antd'
 import './ProductCard_mini.scss'
 
 //TODO add normal classNames or ids
@@ -10,12 +11,12 @@ export function ProductCard_mini({ id, image, title, price }:TCard): JSX.Element
             <Link to={`/product:${id}`}>
                 <section className='info'>
                     <img src={image} alt="Img" />
-                    <h3>{title}</h3>
-                    <h3>{price} р</h3>
+                    <h3 className='line-limit-length'>{title}</h3>
+                    <h3 className='price'>{price} ₽</h3>
                 </section>
             </Link>
             <div className='action'>
-                <button>В корзину</button>
+                <Button type='primary' className='btn_in_cart'>В корзину</Button>
                 <img src="EmptyHeart.svg" alt="favorite" />
             </div>
         </div>
