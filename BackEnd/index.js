1120adea3897eafe10c67cebb73864f76c330919
Arkadashim <andrewsha.antonov@mail.ru>
2022-11-14 20:31:06 +0300

    Update server config

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,2 +1,15 @@
 const express = require('express');
 const path = require('path');
+
+const { Sequelize } = require('./config/database'); // connection object
+const db = require('./config/database'); // db context
+
+//testing db
+db.authenticate()
+    .then(() => console.log('Connected'))
+    .catch(err => console.log(err));
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
