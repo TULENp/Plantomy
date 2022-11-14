37c0a53ad89d7fc4bde2b3ab6d1251b9d18840b2
Eugene <zh_kononenko3@mail.ru>
2022-11-14 10:38:20 +0300

    Fix Filters.tsx dropdown and cashpo -> cachepot

---
+++ FrontEnd/src/components/Filters/Filters.tsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react'
+import { Button, Dropdown, MenuProps, Select, InputNumber  } from 'antd'
+import Icon, {DownOutlined} from "@ant-design/icons"
+
+import './Filters.scss'
+
+export function Filter(): JSX.Element {
+
+    // items of sort dropdown
+    const items = [
+        { label: 'Популярные', value: 'byPopularity' },
+        { label: 'Новинки', value: 'byNovelty' },
+        { label: 'Сначала дешевые', value: 'cheapFirst' },
+        { label: 'Сначала дорогие', value: 'expensiveFirst' },
+    ];
+    
+    return (
+        <aside className='filter'>
+            <Select  className="dropdown" options={items} defaultValue={items[0].value} />
+            {/* TODO add tabs, mb use antd Tabs or Segmented */}
+            <div className='btn_plants_cachepot'>
+                <Button type='primary' className='btn_plants' icon={<Icon component={() => (<img className='img_plant' src="\src\Assets\plant.svg" />)} />}>Растения</Button>
+                <Button className='cachepot' icon={<Icon component={() => (<img className='cachepot' src="\src\Assets\cachepot.svg" />)} />}>Кашпо</Button>
+            </div>
+            
+            <div className="careComplexity">
+                {/* TODO should be radio */}
+                <h3>Сложность ухода</h3>
+                <img className='img_easy' src="\src\Assets\easy.svg" alt="easy" />
+                <img className='img_middle' src="\src\Assets\middle.svg" alt="middle" />
+                <img className='img_hard' src="\src\Assets\hard.svg" alt="hard" />
+            </div>
+            <div className='price_editor'>
+                <InputNumber className='btn_from' placeholder='258' controls={false}/>
+                <img className='line' src='\src\Assets\Line.svg'/>
+                <InputNumber className='btn_to' placeholder='5688' controls={false}/>
+                <Button className='btn_ok'>ок</Button>
+            </div>
+            
+        </aside>
+    )
+}
