cac09a673cf310930d1d58110dbeaf063cb1f68e
Eugene <zh_kononenko3@mail.ru>
2022-11-14 16:51:13 +0300

    Add news carousel to MainPage. Fix News.tsx Add TNews type. Add news placeholder images

---
+++ FrontEnd/src/components/NewsItem/NewsItem.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { TNews } from '../../types'
+// import './style.css'
+
+//* Function of this component:
+//*
+//* This is a news item
+//* click on item takes to page indicated in the link
+//*
+export function NewsItem({ link, image }: TNews): JSX.Element {
+    return (
+        // <img src="News.png" alt="News" id='news'/>
+        <section className='news'>
+            <a href={link} target='_blank' >
+                <img className='news_img' src={image} alt="news" />
+            </a>
+        </section>
+    )
+}
--- FrontEnd/src/pages/MainPage/MainPage.tsx
+++ FrontEnd/src/pages/MainPage/MainPage.tsx
@@ -1,16 +1,33 @@
 import { Filter } from '../../components/Filters';
-import { News } from '../../components/News';
+import { News as NewsItem } from '../../components/NewsItem';
 import { Products } from '../../components/Products';
 import { SearchBar } from '../../components/SearchBar';
 import React from 'react'
 import './MainPage.scss';
+import { Carousel } from 'antd';
+import { CarouselRef } from 'antd/lib/carousel';
+import Icon, { NodeExpandOutlined } from '@ant-design/icons';
 
 export function MainPage(): JSX.Element {
+    // list for news carousel
+    const news = [
+        <NewsItem key={1} link={'https://www.meme-arsenal.com/memes/17673feff1cc6e1fb7ce1a79d12d338b.jpg'} image={'news1.jpg'} />,
+        <NewsItem key={2} link={'https://www.meme-arsenal.com/memes/17673feff1cc6e1fb7ce1a79d12d338b.jpg'} image={'news2.jpg'} />,
+        <NewsItem key={3} link={'https://www.meme-arsenal.com/memes/17673feff1cc6e1fb7ce1a79d12d338b.jpg'} image={'news3.jpg'} />,
+        <NewsItem key={3} link={'https://www.meme-arsenal.com/memes/17673feff1cc6e1fb7ce1a79d12d338b.jpg'} image={'news4.png'} />
+    ];
     return (
         <>
-            <div>MainPage</div>
             <SearchBar />
-            <News />
+            <section className='news_slider'>
+                <Carousel
+                    autoplay
+                    autoplaySpeed={4000} draggable={true}
+                    arrows={true} nextArrow={<Icon component={() => (<img className='img_cachepot' src="right-arrow.svg" />)} />}
+                >
+                    {news}
+                </Carousel>
+            </section>
             <div className="products">
                 <Filter />
                 <Products />
