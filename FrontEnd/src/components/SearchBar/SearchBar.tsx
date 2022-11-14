a4640ea4de4241b8b8580d041a9888140cf3a68f
kildan <kildan325@gmail.com>
2022-11-14 16:20:23 +0300

    Added a searcbar

--- FrontEnd/src/components/SearchBar/SearchBar.tsx
+++ FrontEnd/src/components/SearchBar/SearchBar.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import { Input, Space } from 'antd'
+import './SearchBar.scss';
+const { Search } = Input;
 
 export function SearchBar(): JSX.Element {
     return (
-        <input type="text" name="search" id="search" placeholder='SearchBar' />
+        // <input type="text" name="search" id="search" placeholder='SearchBar' />
+            <Search placeholder="Поиск на Plantomy" size="large" style={{width:665}} enterButton />
+        
     )
 }
--- FrontEnd/src/pages/MainPage/MainPage.tsx
+++ FrontEnd/src/pages/MainPage/MainPage.tsx
@@ -4,12 +4,17 @@ import { Products } from '../../components/Products';
 import { SearchBar } from '../../components/SearchBar';
 import React from 'react'
 import './MainPage.scss';
+import { Button } from 'antd';
+import Icon from "@ant-design/icons"
 
 export function MainPage(): JSX.Element {
     return (
         <>
             <div>MainPage</div>
-            <SearchBar />
+                <div className='search_bar'>
+                    <Button  className="btn_category"type='primary' icon={<Icon component={() => (<img className='img_leaf' src="\src\Assets\leafwhite.svg" />)} />} >Категории</Button>
+                    <SearchBar />
+                </div>
             <News />
             <div className="products">
                 <Filter />
