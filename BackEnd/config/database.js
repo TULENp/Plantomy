568b1283b7153a76138cf961ee75bdf015242f4e
Arkadashim <andrewsha.antonov@mail.ru>
2022-11-14 20:32:22 +0300

    install sequelize-cli, set db-config

--- BackEnd/config/database.js
+++ BackEnd/config/database.js
@@ -1,8 +1,11 @@
 const { Sequelize } = require('sequelize');
+const config = require('./config.json'); // getting config from json-file
+const confenv = config.development; // to set an using environment (dev / test / prod)
+
 module.exports = new Sequelize({
-    database: 'plantomydb',
-    username: 'postgres',
-    password: 'rootroot',
-    host: 'localhost',
-    dialect: 'postgres'
+    database: confenv.database,
+    username: confenv.username,
+    password: confenv.password,
+    host: confenv.host,
+    dialect: confenv.dialect,
 });
