17e28b5007c65387a487ff22679da915b8294fb5
Eugene <zh_kononenko3@mail.ru>
2022-11-17 21:08:03 +0300

    Add cachepotPage and route to it. Change filter

--- FrontEnd/src/pages/CachepotPage/CachepotPage.tsx
+++ FrontEnd/src/pages/CachepotPage/CachepotPage.tsx
@@ -2,11 +2,12 @@ import { Filter } from '../../components/Filters';
 import { News as NewsItem } from '../../components/NewsItem';
 import { Products } from '../../components/Products';
 import { SearchBar } from '../../components/SearchBar';
-import './MainPage.scss';
+import React from 'react'
+import './CachepotPage.scss';
 import { Carousel } from 'antd';
-import Icon from '@ant-design/icons';
+import Icon, { NodeExpandOutlined } from '@ant-design/icons';
 
-export function MainPage(): JSX.Element {
+export function CachepotPage(): JSX.Element {
     // list for news carousel
     const news = [
         <NewsItem key={1} link={'https://www.meme-arsenal.com/memes/17673feff1cc6e1fb7ce1a79d12d338b.jpg'} image={'news1.jpg'} />,
@@ -21,15 +22,14 @@ export function MainPage(): JSX.Element {
                 <Carousel
                     autoplay autoplaySpeed={4000} draggable={true} arrows={true}
                     nextArrow={<Icon component={() => (<img className='img_rightArrow' src="right-arrow.svg" />)} />}
-                    prevArrow={<Icon component={() => (<img className='img_leftArrow' src="left-arrow.svg" />)} />}
-                >
+                    prevArrow={<Icon component={() => (<img className='img_leftArrow' src="left-arrow.svg" />)} />} >
                     {news}
                 </Carousel>
             </section>
             <div className="products">
                 <Filter />
                 {/*TODO mb use list for products */}
-                <Products plants={true}/>
+                <Products plants={false} />
             </div>
         </>
     )
