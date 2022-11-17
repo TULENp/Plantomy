56ced100cb3c807067a9c40b1d49425f8241c0cd
kildan <kildan325@gmail.com>
2022-11-17 21:38:14 +0300

    Add footer

--- FrontEnd/src/components/Footer/Footer.tsx
+++ FrontEnd/src/components/Footer/Footer.tsx
@@ -1,12 +1,28 @@
 import React from 'react'
 import { Logo } from '../Logo'
-import './style.css'
+import { Link } from 'react-router-dom'
+import './Footer.scss'
 
 export function Footer(): JSX.Element {
     return (
-        <footer>
-            <Logo />
-            <h3 className="copyright">© Plantomy 2022. Все права защищены</h3>
-        </footer>
+        <> 
+            <hr></hr>
+            <footer>
+                <div className='logo_copyright'>
+                    <img src="src\Assets\Logo1PNG.png" 
+                        className='logo_img1' 
+                        alt="logo.jpg" />
+                    <h3 className="copyright">© Plantomy 2022. Все права защищены</h3>
+                </div>
+                <Link to={"/"}>
+                    <div className='telegram'>
+                        <img src="src\Assets\tgLogo.svg" 
+                            className='logo_tg' 
+                            alt="tgLogo.svg" />
+                        <h3 className="chat_bot">Чат-бот</h3>
+                    </div>
+                </Link>
+            </footer>
+        </>
     )
 }
--- FrontEnd/vite.config.ts
+++ FrontEnd/vite.config.ts
@@ -3,5 +3,29 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()]
+	plugins: [react()],
+	// module.exports = {
+	// 	rules: [{
+	// 	  test: /\.less$/,
+	// 	  use: [{
+	// 		loader: 'style-loader',
+	// 	  }, {
+	// 		loader: 'css-loader', // translates CSS into CommonJS
+	// 	  }, {
+	// 		loader: 'less-loader', // compiles Less to CSS
+	//        options: {
+	//         lessOptions: { // If you are using less-loader@5 please spread the lessOptions to options directly
+	//           modifyVars: {
+	//             'primary-color': '#1DA57A',
+	//             'link-color': '#1DA57A',
+	//             'border-radius-base': '2px',
+	//           },
+	//           javascriptEnabled: true,
+	//         },
+	//        },
+	// 	  }],
+		  
+	// 	}],
+		
+	//   }
 })
