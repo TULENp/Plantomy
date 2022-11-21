654fa3994a8d2cd9d3b570fe0e32bd126eed5296
Arkadashim <andrewsha.antonov@mail.ru>
2022-11-21 23:24:31 +0300

    Update auth.js

--- BackEnd/controllers/auth.js
+++ BackEnd/controllers/auth.js
@@ -3,11 +3,13 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const config = require('../config/config.json');
 const account = require('../models/account');
+const eH = require('../ustils/errorHandler');
 var Account = models.Account;
 var User = models.User;
 
 module.exports.login = async function(req, res) {
-    const candidate = await Account.findOne({raw: true, where: {Login: req.body.login}});
+    try {
+        const candidate = await Account.findOne({raw: true, where: {Login: req.body.login}});
     if (candidate) {
         const pw = bcrypt.compareSync(req.body?.hash, candidate.Hash);
         if (pw) {
@@ -17,30 +19,37 @@ module.exports.login = async function(req, res) {
             }, config.jwt, {expiresIn: 60*60});
 
             res.status(200).json({
-                token: token,
+                token: `Bearer ${token}`,
             });
         } else {
-            return res.status(401);
+            return res.status(401).json({message:"None"});
         }
     } else {
-        return res.status(401);
+        return res.status(401).json({message:"None"});
+    }
+    } catch(err) {
+        eH(res, err);
     }
 }
 
 module.exports.resgister = async function(req, res) {
-    const candidate = await Account.findOne({raw: true, where: {Login: req.body.login}});
-    if (candidate) {
-        return res.status(409);
+    try {
+        const candidate = await Account.findOne({raw: true, where: {Login: req.body.login}});
+        if (candidate) {
+            return res.status(409);
+        }
+
+        const salt = bcrypt.genSaltSync(10);
+        const pw = req.body?.hash;
+
+        const _account = Account.build({ Login: req.body?.login, Hash: bcrypt.hashSync(pw, salt) });
+        await _account.save().then(() => {
+
+            console.log("Account created");
+            const _user = User.create({AccountId: _account.id, id: _account.id});
+
+        });
+    } catch(err) {
+        eH(res, err);
     }
-
-    const salt = bcrypt.genSaltSync(10);
-    const pw = req.body?.hash;
-
-    const _account = Account.build({ Login: req.body?.login, Hash: bcrypt.hashSync(pw, salt) });
-    await _account.save().then(() => {
-
-        console.log("Account created");
-        const _user = User.create({AccountId: _account.id, id: _account.id});
-
-    }).catch((err) => console.log(err));
 }
