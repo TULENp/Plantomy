1357005cdaf32f7340b97c9ec5e3666cd508303c
Eugene <zh_kononenko3@mail.ru>
2022-11-21 18:41:55 +0300

    Update to Antd v 5.0.0

--- FrontEnd/src/main.tsx
+++ FrontEnd/src/main.tsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import 'normalize.css'
-import 'antd/dist/antd.css'
+import 'antd/dist/reset.css'
 import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
-		<BrowserRouter>
+		<BrowserRouter></BrowserRouter>
 			<App />
 		</BrowserRouter>
 	</React.StrictMode>
