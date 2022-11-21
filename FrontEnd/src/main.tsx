e718c19fe33f4288a621502766d7b3e5f8722a15
Eugene <zh_kononenko3@mail.ru>
2022-11-21 18:52:00 +0300

    reinstall Antd 5

--- FrontEnd/src/main.tsx
+++ FrontEnd/src/main.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
-		<BrowserRouter></BrowserRouter>
+		<BrowserRouter>
 			<App />
 		</BrowserRouter>
 	</React.StrictMode>
