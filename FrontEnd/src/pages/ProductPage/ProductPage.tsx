d03513c209935437603ff5d6fd9e273a2ff76ef0
Eugene <zh_kononenko3@mail.ru>
2022-11-23 22:41:37 +0300

    Fix ProductPage.

--- FrontEnd/src/pages/ProductPage/ProductPage.tsx
+++ FrontEnd/src/pages/ProductPage/ProductPage.tsx
@@ -4,22 +4,19 @@ import { Accessories } from '../../components/Accessories'
 import { ProductCard } from '../../components/ProductCard'
 import { data } from '../../zDataExamples/Data'
 import { TProduct } from '../../types'
-// import './PlantPage.scss'
 
 export function ProductPage(): JSX.Element {
 
     //TODO get selected product (card) from db
-    
-    const [cards, setCards] = useState<TProduct[]>(data);
 
     const { id } = useParams();
-    const productID: number = id ? (+id.split(":")[1] - 1) : 0; // FIXME
-    const card = cards[productID];
+    const productID: number = id ? (+id.split(":")[1]) : 0; 
+    const product: TProduct = data.find(item => item.id === productID) || data[0];
 
     return (
         <article>
-            <ProductCard key={card.id} {...card} />
-            {/* <Accessories /> */}
+            <ProductCard key={product.id} {...product} />
+            <Accessories />
         </article>
     )
 }
