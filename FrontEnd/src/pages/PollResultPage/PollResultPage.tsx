ba6a5b4cb763531b079df7ff5a179cc8b23ba510
Eugene <zh_kononenko3@mail.ru>
2022-11-30 21:28:20 +0300

    Add result page filter logic (DIRTY HACK). Work only after MainPage loaded

--- FrontEnd/src/pages/PollResultPage/PollResultPage.tsx
+++ FrontEnd/src/pages/PollResultPage/PollResultPage.tsx
@@ -1,30 +1,63 @@
 
-import { TChars } from "../../types";
+import { ProductCard } from "../../components/ProductCard";
+import { useAppSelector } from "../../hooks/redux";
+import { TChars, TProduct } from "../../types";
+import { PlantsChars } from "../../zDataExamples/PlantsChars";
 
 export function PollResultPage(): JSX.Element {
 
     const raw: string | null = localStorage.getItem('chars');
     const chars: TChars = raw ? JSON.parse(raw) : null;
+    //! Available only after MainPage loaded
+    const { products } = useAppSelector(state => state.ProductReducer);
+
+    // find ids of plants with suitable characteristics
+    const prodChars = PlantsChars.filter(item =>
+        item.cost === chars.cost &&
+        item.fertilization === chars.fertilization &&
+        item.humidity === chars.humidity &&
+        item.lighting === chars.lighting &&
+        item.preferences === chars.preferences &&
+        item.size === chars.size &&
+        item.temperature === chars.temperature &&
+        item.watering === chars.watering
+    ).map(x => x.id).slice(0, 10);
+
+
+    let prods: TProduct[] = [];
+    //find plants by ids
+    if (prodChars.length !== 0) {
+        for (let i = 0; i < prodChars.length; i++) {
+            prods.push(products.find(item => item.id === prodChars[i])!);
+        }
+    }
+
+    const cardsList: JSX.Element[] = prods.map((prod: TProduct) => {
+        return (
+            <ProductCard product={prod} cardType={'mini'} />
+        )
+    })
+
     return (
         <article>
             <div>PollResultPage</div>
-            <ul>
-                {!chars
-                    ?
-                    <h1>Вы еще не прошли опрос</h1>
-                    :
-                    <>
-                        <li>cost: {chars.cost}</li>
-                        <li>fertilization: {chars.fertilization}</li>
-                        <li>humidity: {chars.humidity}</li>
-                        <li>lighting: {chars.lighting}</li>
-                        <li>preferences: {chars.preferences}</li>
-                        <li>size: {chars.size}</li>
-                        <li>temperature: {chars.temperature}</li>
-                        <li>watering: {chars.watering}</li>
-                    </>
-                }
-            </ul>
+            {!chars
+                ?
+                <h1>Вы еще не прошли опрос</h1>
+                :
+                <>
+                    {prodChars.length !== 0
+                        ?
+                        <>
+                            <ProductCard product={prods[0]} cardType="cart" />
+                            <h1>Также вам подходят</h1>
+                            {cardsList.slice(1)}
+                        </>
+                        :
+                        <h1>Подходящего растения нет</h1>
+                    }
+                </>
+            }
         </article>
     )
 }
--- FrontEnd/src/zDataExamples/PlantsChars.ts
+++ FrontEnd/src/zDataExamples/PlantsChars.ts
@@ -1,6 +1,6 @@
 export const PlantsChars = [
     {
-        id:1,
+        id: 1,
         watering: 1,
         lighting: 1,
         temperature: 1,
@@ -11,18 +11,18 @@ export const PlantsChars = [
         cost: 1
     },
     {
-        id:2,
-        watering: 2,
-        lighting: 2,
-        temperature: 2,
-        humidity: 2,
-        fertilization: 2,
-        size: 2,
-        preferences: 2,
-        cost: 2
+        id: 2,
+        watering: 1,
+        lighting: 1,
+        temperature: 1,
+        humidity: 1,
+        fertilization: 1,
+        size: 1,
+        preferences: 1,
+        cost: 1
     },
     {
-        id:3,
+        id: 3,
         watering: 3,
         lighting: 3,
         temperature: 3,
@@ -33,7 +33,7 @@ export const PlantsChars = [
         cost: 3
     },
     {
-        id:4,
+        id: 4,
         watering: 2,
         lighting: 2,
         temperature: 4,
@@ -44,7 +44,7 @@ export const PlantsChars = [
         cost: 0
     },
     {
-        id:5,
+        id: 5,
         watering: 2,
         lighting: 3,
         temperature: 1,
