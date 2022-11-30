a50ffe2cc324a9e3f9d33b1284ca99edf3741666
kildan <kildan325@gmail.com>
2022-11-30 15:24:59 +0300

    Add Quantity button in producCard_mini

--- FrontEnd/src/components/ProductCard_mini/ProductCard_mini.tsx
+++ FrontEnd/src/components/ProductCard_mini/ProductCard_mini.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { TProduct } from '../../types'
 import { Button } from 'antd'
@@ -10,6 +10,24 @@ import './ProductCard_mini.scss'
 //*
 //TODO add normal classNames or ids
 export function ProductCard_mini({ id, image, title, price }:TProduct): JSX.Element {
+    const [quanActive,setQuanActive] = useState(false);
+    const [quanNum, setQuanNum] = useState(1);
+
+    function Increment () {
+        if(quanNum < 99) {
+            setQuanNum(quanNum + 1);
+        }
+    }
+
+    function Decrement () {
+        if(quanNum > 1) {
+            setQuanNum(quanNum - 1);
+        }
+        else{
+            setQuanActive(false);
+        }
+    }
+
     return (
         <div className='ProductCard_mini'>
             <Link to={`/product:${id}`}>
@@ -20,7 +38,17 @@ export function ProductCard_mini({ id, image, title, price }:TProduct): JSX.Elem
                 </section>
             </Link>
             <div className='action'>
-                <Button type='primary' className='btn_in_cart'>В корзину</Button>
+                { quanActive 
+                    ?
+                        <div className='btn_quan'>
+                            <span className='minus' onClick={Decrement}>-</span>
+                            <span className='num'>{quanNum}</span>
+                            <span className='plus' onClick={Increment} >+</span>
+                        </div>
+                    :
+                        <Button type='primary' className='btn_in_cart' onClick={()=> {setQuanActive(true)}}>В корзину</Button>
+                    }
+                
                 <img src="EmptyHeart.svg" alt="favorite" />
             </div>
         </div>
