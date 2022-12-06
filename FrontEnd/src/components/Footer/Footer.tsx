12bfb66373a962839761a31bd7b3cc1e53511c7d
Eugene <zh_kononenko3@mail.ru>
2022-12-06 22:29:15 +0300

    Add link to Telegram Bot

--- FrontEnd/src/components/Footer/Footer.tsx
+++ FrontEnd/src/components/Footer/Footer.tsx
@@ -5,23 +5,23 @@ import './Footer.scss'
 
 export function Footer(): JSX.Element {
     return (
-        <> 
+        <>
             <hr className='line_footer'></hr>
             <footer>
                 <div className='logo_copyright'>
-                    <img src="src\Assets\Logo1PNG.png" 
-                        className='logo_img1' 
+                    <img src="src\Assets\Logo1PNG.png"
+                        className='logo_img1'
                         alt="logo.jpg" />
                     <h3 className="copyright">© Plantomy 2022. Все права защищены</h3>
                 </div>
-                <Link to={"/"}>
+                <a href='https://t.me/PlantomyBot' target='_blank'>
                     <div className='telegram'>
-                        <img src="src\Assets\tgLogo.svg" 
-                            className='logo_tg' 
+                        <img src="src\Assets\tgLogo.svg"
+                            className='logo_tg'
                             alt="tgLogo.svg" />
                         <h3 className="chat_bot">Чат-бот</h3>
                     </div>
-                </Link>
+                </a>
             </footer>
         </>
     )
