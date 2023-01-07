256a2efff76c7c7ddc1dfc4bd5037abe66248978
Eugene <zh_kononenko3@mail.ru>
2023-01-07 23:11:47 +0300

    Add sorter to orderList table. Turn off table tooltips. Add types to table.

--- FrontEnd/src/components/OrdersList/OrdersList.tsx
+++ FrontEnd/src/components/OrdersList/OrdersList.tsx
@@ -1,6 +1,8 @@
 import { Table } from 'antd';
+import { ColumnsType } from 'antd/es/table';
 import { useNavigate } from 'react-router-dom';
 import { useAppSelector } from '../../hooks/redux';
+import { TOrder } from '../../types';
 
 //* Function of this component:
 //*
@@ -11,7 +13,7 @@ export function OrdersList(): JSX.Element {
     const { orders, isLoading, error } = useAppSelector(state => state.OrdersReducer)
     const navigate = useNavigate();
 
-    const cols = [
+    const cols: ColumnsType<TOrder> = [
         {
             title: 'Номер',
             dataIndex: 'id',
@@ -26,7 +28,10 @@ export function OrdersList(): JSX.Element {
         },
         {
             title: 'Оформлен',
-            dataIndex: 'date'
+            dataIndex: 'date',
+            sortDirections: ['ascend', 'descend', 'ascend'],
+            defaultSortOrder: 'descend',
+            sorter: (a: TOrder, b: TOrder) => +new Date(a.date) - +new Date(b.date),
         }
     ];
 
@@ -46,7 +51,7 @@ export function OrdersList(): JSX.Element {
                                 ?
                                 <h1>Список заказов пуст</h1>
                                 :
-                                <Table columns={cols} dataSource={orders}
+                                <Table<TOrder> columns={cols} dataSource={orders} showSorterTooltip={false}
                                     onRow={(record) => {
                                         return {
                                             onClick: () => {
