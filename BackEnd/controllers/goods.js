baafe8f086d55e3b6e51ae9cb389d15a342c82c9
Arkadashim <andrewsha.antonov@mail.ru>
2023-01-09 11:23:20 +0300

    add filters and goods for auth user

--- BackEnd/controllers/goods.js
+++ BackEnd/controllers/goods.js
@@ -1,11 +1,14 @@
 const eH = require('../middleware/errorHandler');
 const { Sequelize } = require('../models');
 const models = require('../models');
+const { Op } = require("sequelize");
 const Product = models.Product;
 const ProdInfo = models.ProductInfo;
 const ProdChars = models.ProductChar;
 const ProdType = models.ProductType;
 const ProdCat = models.Category;
+const Favorite = models.Favorite;
+const Cart = models.Cart;
 
 // request is not used
 module.exports.getAll = async function (req, res) {
@@ -30,6 +33,55 @@ module.exports.getAll = async function (req, res) {
                 [Sequelize.col('createdAt'), 'date'],
             ],
         });
+        res.status(200).json(_goods);
+    } catch (err) {
+        eH(res, err);
+    }
+}
+
+// in header - Authorization
+module.exports.getAllAuth = async function (req,res) {
+    try {
+        const _goods = await Product.findAll({
+            raw: true,
+            include: [{
+                model: ProdType,
+                attributes: [],
+            }, {
+                model: ProdCat,
+                attributes: [],
+            }],
+            attributes: [
+                'id',
+                [Sequelize.col('ProductType.Type'), 'type'],
+                [Sequelize.col('Category.Category'), 'category'],
+                [Sequelize.col('Size'), 'size'],
+                [Sequelize.col('Image'), 'image'],
+                [Sequelize.col('Name'), 'title'],
+                [Sequelize.col('Price'), 'price'],
+                [Sequelize.col('createdAt'), 'date'],
+            ],
+        });
+        
+        let userId = req.user.id;
+        const _favs = await Favorite.findAll({raw: true, where: {UserId: userId}});
+        const _cart = await Cart.findAll({raw: true, where: {UserId: userId}});
+
+        for(var k in _goods) {
+            _goods[k].isFav = false;
+            for (var l in _favs) {
+                if (_goods[k].id === _favs[l].ProductId) {
+                    _goods[k].isFav = true;
+                }
+            }
+            _goods[k].cartCount = 0;
+            for (var l in _cart) {
+                if (_goods[k].id === _cart[l].ProductId) {
+                    _goods[k].cartCount = _cart[l].Count;
+                }
+            }
+        }
+
         res.status(200).json(_goods);
     } catch (err) {
         eH(res, err);
@@ -136,6 +188,80 @@ module.exports.getByFilters = async function (req, res) {
                 [Sequelize.col('Product.createdAt'), 'date']
             ],
             where: brief,
+        });
+        res.status(200).json(_goods);
+    } catch (err) {
+        eH(res, err);
+    }
+}
+
+/*
+    need to send body like: 
+
+    {
+        "search": 'sasi'
+        "cost": {
+            "min": 228,
+            "max": 1337
+        },
+        "type": 1,
+        "sort": 0,
+        "category": 2
+    }
+
+    if something is not needed - send "null" or do not send ever. 
+    for type and category - use ids from database.
+    for "sort": популярные - 0, новинки - 1, сн дешевые - 2, сн дорогие - 3. in case of null - 0.
+*/
+module.exports.getByInterfaceFilters = async function (req, res) {
+    try {
+        const filters = req.body;
+        const _where = {};
+        const _order = [['id'], ['createdAt'], ['Price'], [['Price','DESC']]];
+
+        if (filters.search) {
+            _where.Name = {[Op.iLike]: '%'+filters.search+'%'};
+        }
+
+        if (filters.cost) {
+            _where.Price = {[Op.and]: {}};
+            if (filters.cost.min) {
+                _where.Price[Op.and][Op.gte] = filters.cost.min;
+            }
+            if (filters.cost.max) {
+                _where.Price[Op.and][Op.lte] = filters.cost.max;
+            }
+        }
+
+        if (filters.type) {
+            _where.ProductTypeId = filters.type;
+        }
+
+        if (filters.category) {
+            _where.CategoryId = filters.category;
+        }
+
+        let _sort = _order[filters.sort];
+
+        const _goods = await Product.findAll({raw: true, where: _where,
+            include: [{
+                model: ProdType,
+                attributes: [],
+            }, {
+                model: ProdCat,
+                attributes: [],
+            }],
+            attributes: [
+                'id',
+                [Sequelize.col('ProductType.Type'), 'type'],
+                [Sequelize.col('Category.Category'), 'category'],
+                [Sequelize.col('Size'), 'size'],
+                [Sequelize.col('Image'), 'image'],
+                [Sequelize.col('Name'), 'title'],
+                [Sequelize.col('Price'), 'price'],
+                [Sequelize.col('createdAt'), 'date'],
+            ],
+            order: _sort,
         });
         res.status(200).json(_goods);
     } catch (err) {
--- BackEnd/routes/goods.js
+++ BackEnd/routes/goods.js
@@ -4,7 +4,9 @@ const controller = require('../controllers/goods');
 const passport = require('passport');
 
 router.get('/getAll', controller.getAll);
+router.get('/getAllAuth', passport.authenticate('jwt', {session: false}), controller.getAllAuth);
 router.post('/getByFilter', controller.getByFilters);
+router.post('/getByInterfaceFilters', controller.getByInterfaceFilters);
 router.get('/', controller.get);
 
 module.exports = router;
