a57ab12f5b58e96746f1a8b4cce42c86029a7930
Arkadashim <andrewsha.antonov@mail.ru>
2023-01-12 00:17:23 +0300

    честно - честно

--- BackEnd/routes/cart.js
+++ BackEnd/routes/cart.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const controller = require('../controllers/cart');
 const passport = require('passport');
 
-router.post('/addtoCart', passport.authenticate('jwt', {session: false}), controller.addToCart);
-router.post('/dropfromCart', passport.authenticate('jwt', {session: false}), controller.dropFromCart);
+router.post('/addToCart', passport.authenticate('jwt', {session: false}), controller.addToCart);
+router.post('/dropFromCart', passport.authenticate('jwt', {session: false}), controller.dropFromCart);
 router.post('/incGoods', passport.authenticate('jwt', {session: false}), controller.incGoods);
 router.post('/decGoods', passport.authenticate('jwt', {session: false}), controller.decGoods);
 router.get('/getCart', passport.authenticate('jwt', {session: false}), controller.getCart);
