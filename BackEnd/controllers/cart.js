aba5986d5442e1331fff02a04853b37363da4c0d
Arkadashim <andrewsha.antonov@mail.ru>
2023-01-12 22:11:49 +0300

    some bug fix

--- BackEnd/controllers/cart.js
+++ BackEnd/controllers/cart.js
@@ -6,105 +6,113 @@ const Cart = models.Cart;
 const Fav = models.Favorite;
 
 // should contain in req: in header - Authorization, in body - ProductId
-module.exports.addToCart = async function(req, res) {
+module.exports.addToCart = async function (req, res) {
     try {
         const prId = req.body.productId;
-        const _cart = await Cart.findOne({ raw:true, where: {
-            UserId: req.user.id,
-            ProductId: prId,
-        }});
+        const _cart = await Cart.findOne({
+            raw: true, where: {
+                UserId: req.user.id,
+                ProductId: prId,
+            }
+        });
         if (_cart) {
             res.status(400).end();
         } else {
-            let _prod = await Product.findOne({raw: true, where: {id: prId}, attributes: ['Count']});
-            if (_prod.Count<1) {
-                res.status(202).json({message: 'Товара нет на складе'});
+            let _prod = await Product.findOne({ raw: true, where: { id: prId }, attributes: ['Count'] });
+            if (_prod.Count < 1) {
+                res.status(202).json({ message: 'Товара нет на складе' });
             } else {
-                await Cart.create({UserId: req.user.id, ProductId: prId, Count: 1});
-                res.status(200).json({message: 'Товар добавлен в корзину!'});
+                await Cart.create({ UserId: req.user.id, ProductId: prId, Count: 1 });
+                res.status(200).json({ message: 'Товар добавлен в корзину!' });
             }
         }
-    } catch(err) {
+    } catch (err) {
         eH(res, err);
     }
 }
 
 // should contain in req: in header - Authorization, in body - ProductId
-module.exports.dropFromCart = async function(req, res) {
+module.exports.dropFromCart = async function (req, res) {
     try {
         const prId = req.body.productId;
-        const _cart = await Cart.findOne({ raw:true, where: {
-            UserId: req.user.id,
-            ProductId: prId,
-        }});
+        const _cart = await Cart.findOne({
+            raw: true, where: {
+                UserId: req.user.id,
+                ProductId: prId,
+            }
+        });
         if (_cart) {
-            await Cart.destroy({raw:true, where: {id: _cart.id}});
-            res.status(200).json({message: 'Товар удалён из корзины!'});
+            await Cart.destroy({ raw: true, where: { id: _cart.id } });
+            res.status(200).json({ message: 'Товар удалён из корзины!' });
         } else {
             res.status(400).end();
         }
-    } catch(err) {
-        eH(res,err);
+    } catch (err) {
+        eH(res, err);
     }
 }
 
 // should contain in req: in header - Authorization, in body - ProductId
-module.exports.incGoods = async function(req, res) {
+module.exports.incGoods = async function (req, res) {
     try {
         const prId = req.body.productId;
-        const _cart = await Cart.findOne({ raw:true, where: {
-            UserId: req.user.id,
-            ProductId: prId,
-        }});
+        const _cart = await Cart.findOne({
+            raw: true, where: {
+                UserId: req.user.id,
+                ProductId: prId,
+            }
+        });
 
         if (_cart) {
-            let _prod = await Product.findOne({raw: true, where: {id: prId}, attributes: ['Count']});
+            let _prod = await Product.findOne({ raw: true, where: { id: prId }, attributes: ['Count'] });
             if (_cart.Count + 1 <= _prod.Count) {
-                await Cart.update( 
-                    {Count: ++_cart.Count}, 
-                    {where:{id: _cart.id}});
-                res.status(200).json({message: 'Товар добавлен!'});
+                await Cart.update(
+                    { Count: ++_cart.Count },
+                    { where: { id: _cart.id } });
+                res.status(200).json({ message: 'Товар добавлен!' });
             } else {
-                res.status(202).json({message: 'Недостаточно товара на складе'});
+                res.status(202).json({ message: 'Недостаточно товара на складе' });
             }
         } else {
             res.status(400).end();
         }
 
-    } catch(err) {
-        eH(res,err);
+    } catch (err) {
+        eH(res, err);
     }
 }
 
 // should contain in req: in header - Authorization, in body - ProductId
-module.exports.decGoods = async function(req, res) {
+module.exports.decGoods = async function (req, res) {
     try {
         const prId = req.body.productId;
-        const _cart = await Cart.findOne({ raw:true, where: {
-            UserId: req.user.id,
-            ProductId: prId,
-        }});
-        if (_cart.Count <= 1) {
-            res.status(204).end();
-        } else {
-            await Cart.update( 
-                {Count: --_cart.Count}, 
-                {where:{id: _cart.id}});
+        const _cart = await Cart.findOne({
+            raw: true, where: {
+                UserId: req.user.id,
+                ProductId: prId,
             }
-        res.status(200).json({message: 'Товар убран!'});
-    } catch(err) {
-        eH(res,err);
+        });
+        if (_cart.Count <= 1) {
+            res.status(200).json({ message: 'Нельзя уменьшить!' });
+        } else {
+            await Cart.update(
+                { Count: --_cart.Count },
+                { where: { id: _cart.id } });
+            res.status(200).json({ message: 'Товар убран!' });
+        }
+    } catch (err) {
+        eH(res, err);
     }
 }
 
 // should contain in req: in header - Authorization
-module.exports.getCart = async function(req, res) {
+module.exports.getCart = async function (req, res) {
     try {
 
         const _cart = await Cart.findAll({
             raw: true,
-            include: {model: Product, attributes: []},
-            where: {UserId: req.user.id},
+            include: { model: Product, attributes: [] },
+            where: { UserId: req.user.id },
             attributes: [
                 [Sequelize.col('Product.id'), 'id'],
                 [Sequelize.col('Product.Image'), 'image'],
@@ -116,13 +124,13 @@ module.exports.getCart = async function(req, res) {
 
         if (_cart) {
             for (var k in _cart) {
-                var _fav = await Fav.findOne({raw: true, where: {ProductId: _cart[k].id}});
+                var _fav = await Fav.findOne({ raw: true, where: { ProductId: _cart[k].id } });
                 _cart[k].isFav = _fav !== null;
             }
         }
 
         res.status(200).json(_cart);
-    } catch(err) {
-        eH(res,err);
+    } catch (err) {
+        eH(res, err);
     }
 }
