4c9e0e7ac53499b53346797b1fdc2361d04f5d7b
Eugene <zh_kononenko3@mail.ru>
2023-02-03 13:54:07 +0300

    Comment proxy in vite.config

--- FrontEnd/vite.config.ts
+++ FrontEnd/vite.config.ts
@@ -4,14 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	server: {
-		proxy: {
-			"/api": {
-				target: "https://plantomy-backend.vercel.app/api",
-				changeOrigin: true,
-				secure: false,
-				rewrite: (path) => path.replace(/^\/api/, ""),
-			},
-		},
-	},
+	// server: {
+	// 	proxy: {
+	// 		"/api": {
+	// 			target: "https://plantomy-backend.vercel.app/api",
+	// 			changeOrigin: true,
+	// 			secure: false,
+	// 			rewrite: (path) => path.replace(/^\/api/, ""),
+	// 		},
+	// 	},
+	// },
 })
